7520736021a4d389a2e82e2fa6aabdf76c776739
PSkrabneu <skrebnev@gmail.com>
2024-12-17 14:11:57 +0100

    Changes in config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from "nuxt";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-<<<<<<< HEAD
-=======
   future: {
     compatibilityVersion: 4,
   },
->>>>>>> 691d911c85830c2e8339f4cf64d8be9390a9b2f4
 });
