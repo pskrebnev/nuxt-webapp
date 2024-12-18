2caae6c1da031207f831d9b13e0903affe9825e4
PSkrabneu <skrebnev@gmail.com>
2024-12-18 17:54:14 +0100

    Style changed

--- portfolio/app/layouts/default.vue
+++ portfolio/app/layouts/default.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <nav>
-      <li>
-        <NuxtLink to="/">Main Page</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about">About Page</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/blog">My blog Page</NuxtLink>
-      </li>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+          >Home</NuxtLink
+        >
+      </div>
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li>
+            <NuxtLink to="/">Main Page</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About Page</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/blog">My blog Page</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <slot name="header" />
+    </header>
+    <main class="p-4">
+      <slot />
+    </main>
   </div>
 </template>
