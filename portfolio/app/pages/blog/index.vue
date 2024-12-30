2d9d1b292171a292019bab3256695f020a6cd1b3
PSkrabneu <skrebnev@gmail.com>
2024-12-30 15:58:51 +0100

    Blog features implemented

--- portfolio/app/pages/blog/index.vue
+++ portfolio/app/pages/blog/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>I'm the blog. And here is the list of all blog posts</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+const { data: posts } = await useAsyncData("blog-list", () =>
+  queryContent("/blog").only(["_path", "title"]).find()
+);
+console.log(posts);
+</script>
